edac4d283b435cfeb7369151e50b28fe71a71cad
kali <kali@kali.eh>
2022-02-08 20:01:42 +0100

    last.py bases are ok.

--- last.py
+++ last.py
@@ -1,12 +1,22 @@
 import requests
+from sympy import N
 from controller import *
+import json 
 
-def currentweather() :
-    url = urlAPI+"/last/10"
+def last(n) :
+    
+    number = n
+    i = 0
+    url = urlAPI+"last/"+str(number)
 
     resp = requests.get(url=url)
     data = resp.json() 
-
-    return data
-
-print (currentweather())
+    
+    a = ""
+    for i in range(number) : 
+        a+=data[i]["id"]+"\n"
+        a+=data[i]["summary"]+"\n"
+        i = i + 1 
+        # print (i)
+    return a 
+print(last(3))
