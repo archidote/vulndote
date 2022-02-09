0ffa3339691687a1b2977e6dc564c6bd16cbb054
kali <kali@kali.eh>
2022-02-09 16:45:30 +0100

    ConfigurationAffectedByTheVuln() ok, terminology() is under dev.

--- last.py
+++ last.py
@@ -1,9 +1,10 @@
 import requests
-from sympy import N
 from controller import *
-import json 
 
 def last(n) :
+    
+    if n >= 10 : 
+        return "Unable to display more than 10 inputs"
     
     number = n
     i = 0
@@ -12,12 +13,18 @@ def last(n) :
     resp = requests.get(url=url)
     data = resp.json() 
     
-    a = ""
+    cve = ""
     for i in range(number) : 
-        a+=data[i]["id"]+"\n"
-        a+=data[i]["summary"]+"\n"
+        cve+=data[i]["id"]+"\n\n"
+        cve+="Published : "+data[i]["Published"]+"\n"
+        cve+="Edited : "+data[i]["Modified"]+"\n"
+        cve+="CVSS Score : "+str(data[i]["cvss"])+"\n\n"
+        cve+="Summary : "+data[i]["summary"]+"\n\n"
+        cve+="Vulnerable Product : \n"+data[i]["vulnerable_product"][i]+"\n\n"
         i = i + 1 
-        # print (i)
-    return a 
+    return cve
+
+def lastInRealTime(): 
+    return ""
 
 print(last(3))
