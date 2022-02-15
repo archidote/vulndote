ac5fe9e2bcd029d23ea9945a968eb0e4e4e6f289
kali <kali@kali.eh>
2022-02-15 12:01:43 +0100

    api changed. cve.circl.lu/api -> opencve.io/api. creds for this api are stored into the ~/.zshrc file. fetching CVE feature by date (global),vendor,cvss are ok.

--- controller.py
+++ controller.py
@@ -1,1 +1,13 @@
-urlAPI="https://cve.circl.lu/api/"
+import requests
+import os
+
+oldURLApi = "https://cve.circl.lu/api/"
+vulndoteUser = os.environ.get('vulndoteUser')
+vulndotePassword = os.environ.get('vulndotePassword')
+
+# print (vulndoteUser)
+# print (vulndotePassword)
+
+session = requests.Session()
+session.auth = (vulndoteUser, vulndotePassword)
+auth = session.post('https://www.opencve.io/api/')
