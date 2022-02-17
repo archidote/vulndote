a389151900ff36db9e45f48c9cf2756e56d44cf3
kali <kali@kali.eh>
2022-02-17 15:51:32 +0100

    bot's birth

--- todayCVE.py
+++ todayCVE.py
@@ -1,3 +1,4 @@
+from numpy import empty
 from controller import * 
 from datetime import datetime
 from datetime import timezone
@@ -13,15 +14,35 @@ def cveTodaySortedByVendor(vendor) :
     
     cve = ""
     for i in range(len(data)):
-        if today == formatDate(data[i]["updated_at"]):
+        if today in formatDate(data[i]["updated_at"]):
             cve += "CVE ID : "+data[i]["id"]+"\n"
             cve += "Summary : "+data[i]["summary"]+"\n"
             cve += "Published/Updated : "+data[i]["updated_at"]+"\n\n"
-    return cve  
+    if cve : # IF cve variable is not empty 
+        return cve  
+    else : 
+        return "No CVE"
 
 def cveTodaySortedByCVSS(cvss) :  
     
     response = session.get('https://www.opencve.io/api/cve?cvss='+cvss)
+    
+    data = response.json() 
+
+    today = now = datetime.now()
+    today = now.strftime("%Y-%m-%d")
+    
+    cve = ""
+    for i in range(len(data)):
+        if today == formatDate(data[i]["updated_at"]):
+            cve += "CVE ID : "+data[i]["id"]+"\n"
+            cve += "Summary : "+data[i]["summary"]+"\n"
+            cve += "Published/Updated :"+data[i]["updated_at"]+"\n\n"
+    return cve  
+
+def cveTodaySortedByVendorAndCVSS(vendor,cvss) :  
+    
+    response = session.get('https://www.opencve.io/api/cve?vendor='+vendor+'&cvss='+cvss+'')
     
     data = response.json() 
 
@@ -59,6 +80,7 @@ def formatDate(rawDate) :
     return res
 
 
-print (cveTodaySortedByVendor("linux"))
-print (cveTodaySortedByCVSS("critical"))
-print (cveTodayNotSorted())
+# print (cveTodaySortedByVendor("microsoft"))
+# print (cveTodaySortedByCVSS("critical"))
+# print (cveTodayNotSorted())
+# print (cveTodaySortedByVendorAndCVSS("Linux","Low"))
