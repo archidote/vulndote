c5002c75ffefd2bcde729c92bd087d7d82157a8e
kali <kali@kali.eh>
2022-02-22 16:58:45 +0100

    cveReferences() - vulnerableProductsOrVendors() help menu etc.

--- functions.py
+++ functions.py
@@ -1,4 +1,27 @@
+from numpy import product
 from controller import * 
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def cveSearch(cveCode) : 
+    
+    response = session.get('https://www.opencve.io/api/cve/'+cveCode+'')
+    data = response.json() 
+
+    
+    if "message" in data : 
+        return "CVE has'nt been found."
+        
+    else :
+        cve = "" 
+        cve += "*CVE ID*: "+data["id"]+"\n"
+        cve += "*CVSS* : "+cvssScale((data["id"]))+"\n"
+        cve += "*Summary* : "+data["summary"]+"\n"
+        cve += "*Published/Updated* : "+data["updated_at"]+"\n\n"
+        cve += "*More info ?* :  "
+        
+        return cve 
 
 def cvssScale(cve): 
     
@@ -14,20 +37,66 @@ def cvssScale(cve):
     else : 
         return ""+str(data["cvss"]["v3"])+" 🔴"
 
-def cveSearch(cveCode) : 
+def cveReferences(cve) :
     
-    response = session.get('https://www.opencve.io/api/cve/'+cveCode+'')
+    response = session.get('https://www.opencve.io/api/cve/'+cve+'')
     data = response.json() 
 
     
     if "message" in data : 
         return "CVE has'nt been found."
         
-    else : 
-        cve = ""
-        cve += "<b>CVE ID</b> : "+data["id"]+"\n"
-        cve += "<b>CVSS</b> : "+cvssScale((data["id"]))+"\n"
-        cve += "<b>Summary</b> : "+data["summary"]+"\n"
-        cve += "<b>Published/Updated</b> : "+data["updated_at"]+"\n\n"
-        
+    else :
+        cve = "" 
+        cve += "*CVE ID*: "+data["id"]+"\n\n"
+        for i in range(len(data["raw_nvd_data"]["cve"]["references"]["reference_data"])) :
+            cve += data["raw_nvd_data"]["cve"]["references"]["reference_data"][i]["refsource"]+"\n"
+            cve += "   -"+data["raw_nvd_data"]["cve"]["references"]["reference_data"][i]["url"]+"\n"
+            
         return cve 
+
+def vulnerableProductsOrVendors(cve) : 
+    
+    response = session.get('https://www.opencve.io/api/cve/'+cve+'')
+    data = response.json() 
+
+    
+    if "message" in data : 
+        return "CVE has'nt been found."
+        
+    else :
+        cve = "" 
+        cve += "*CVE ID*: "+data["id"]+"\n\n"
+        for key in data["vendors"] :
+            cve += ""+key+" : \n"
+            for v in data["vendors"][key] : 
+                product = ""
+                product += ''.join(v)
+                cve += "  "+product+"\n"
+        return cve 
+            
+def terminology() : 
+    
+    terms = """
+TERMINOLOGY : 
+
+CVE : Common Vulnerabilities and Exposures
+CVSS : Common Vulnerability Scoring System (0->10)
+CVSS-Vector : AV:N/AC:L/Au:N/C:N/I:N/A:P
+    More info cvss-vector (v2): https://www.first.org/cvss/v2/guide 
+    More info cvss-vector (v3): https://www.first.org/cvss/calculator/3.0
+CWE : Common Weakness Enumeration
+    More Info : https://fr.wikipedia.org/wiki/Common_Weakness_Enumeration
+CPE : Common Platform Enumeration
+    More info : https://en.wikipedia.org/wiki/Common_Platform_Enumeration
+    
+    """
+    return terms
+
+def timeOutAPI() : # TO-DO
+    r = session.get("https://example.com", timeout=10000)
+    print (r)
+
+# print(cveReferences("CVE-2021-29987"))
+# print (vulnerableProductsOrVendors("CVE-2017-0144"))
+# print (timeOutAPI())
--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import *
-from time import time 
-#from assets.search import * 
+from time import time
 from todayCVE import * 
 from controller import * 
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -9,6 +8,15 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from todayCVE import *
 
 bot = telebot.TeleBot(telegramBotToken, parse_mode=None)
+
+help = """
+HELP //
+
+/cve 
+/last_cve_list
+/today_cve_for_a_product
+/terminology
+"""
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -20,33 +28,55 @@ def send_welcome(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	markup = telebot.types.ForceReply()
 	bot.reply_to(message, "Enter a CVE code :", reply_markup=markup)
+ 
+@bot.message_handler(commands=['last_cve_list'])
+def send_welcome(message):
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	b1 = InlineKeyboardButton(text='/critical')
+	b2 = InlineKeyboardButton(text='/high')
+	b3 = InlineKeyboardButton(text='/medium')
+	b4 = InlineKeyboardButton(text='/low')
+	markup.add(b1, b2, b3, b4)
+	bot.reply_to(message, "dsqqsd",reply_markup=markup)
+ 
+@bot.message_handler(commands=['critical','high','medium','low'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    asset = message.text 
+    asset = asset.replace('/','')
+    cve = cveTodaySortedByCVSS(asset)
+    print (cve)
+    if len(cve) > 4095:
+        for x in range(0, len(cve), 4095): # Allow vulndote to send big GLOBAL message (split in x messages)
+            bot.reply_to(message, text=cve[x:x+4095],reply_markup=markup, parse_mode="markdown")     
+    else : 
+        print (1)
+        bot.reply_to(message, cve, reply_markup=markup, parse_mode="markdown")
 
-@bot.message_handler(commands=['cve_today_vendor_product'])
+@bot.message_handler(commands=['cve_sorted_today_asset'])
 def send_welcome(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	markup = telebot.types.ForceReply()
-	bot.reply_to(message, "Enter a Vendor/Product", reply_markup=markup)
+	bot.reply_to(message, "eEnter a Vendor :", reply_markup=markup, parse_mode="markdown")
  
-@bot.message_handler(commands=['cve_sorted'])
+@bot.message_handler(commands=['terminology'])
 def send_welcome(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-	markup = telebot.types.ForceReply()
-	bot.reply_to(message, "eEnter a Vendor :", reply_markup=markup)
- 
+	bot.reply_to(message,terminology(), reply_markup=markup, parse_mode="markdown")
  
 ###########################################################################################################
 
 @bot.message_handler(func=lambda m: True)
 def which_reply(message):
 	if message.reply_to_message == None : 
-		bot.reply_to(message, "HELP")
+		bot.reply_to(message,help)
 	else :
 		if message.reply_to_message.text == "Enter a CVE code :" : 
 			markup = InlineKeyboardMarkup()
-			bot.reply_to(message, cveSearch(message.text), reply_markup=markup,parse_mode="html")
-		if message.reply_to_message.text == "Enter a Vendor/Product" :
-			markup = InlineKeyboardMarkup()
-			bot.reply_to(message, cveTodaySortedByVendor(message.text), reply_markup=markup,parse_mode="html")
+			b1 = InlineKeyboardButton(text='Products Affected', callback_data = 'Products_Affected')
+			b2 = InlineKeyboardButton(text='References', callback_data = 'References')
+			markup.add(b1, b2)
+			bot.reply_to(message, cveSearch(message.text), reply_markup=markup, parse_mode="markdown")
 		elif message.reply_to_message.text == "eEnter a Vendor :" : 
 			markup = InlineKeyboardMarkup()
 			b1 = InlineKeyboardButton(text='Critical', callback_data = 'Critical')
@@ -54,22 +84,31 @@ def which_reply(message):
 			b3 = InlineKeyboardButton(text='Medium', callback_data = 'Medium')
 			b4 = InlineKeyboardButton(text='Low', callback_data = 'Low')
 			markup.add(b1, b2, b3, b4)
-			bot.reply_to(message,"Choose :",reply_markup=markup)
+			bot.reply_to(message,cveTodaySortedByVendor(message.text), reply_markup=markup, parse_mode="markdown")
    
 @bot.callback_query_handler(func=lambda call: call.data != 'check_group') # Buttons fetch reply value 
 def callback_inline(call):
 
 	if call.data == "Critical":
-		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"Critical"), reply_markup=call.message.reply_markup,parse_mode="html")
-		bot.answer_callback_query(call.id, " Loading... ")
+		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"critical"), reply_markup=call.message.reply_markup, parse_mode="markdown")
+		bot.answer_callback_query(call.id, "Loading... ")
 	if call.data == "High":
-		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"High"), reply_markup=call.message.reply_markup,parse_mode="html")
+		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"high"), reply_markup=call.message.reply_markup, parse_mode="markdown")
 		bot.answer_callback_query(call.id, "Loading...")
 	if call.data == "Medium":
-		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"Medium"), reply_markup=call.message.reply_markup,parse_mode="html")
+		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"medium"), reply_markup=call.message.reply_markup, parse_mode="markdown")
 		bot.answer_callback_query(call.id, "Loading...")
 	if call.data == "Low":
-		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"Low"), reply_markup=call.message.reply_markup,parse_mode="html")
+		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"low"), reply_markup=call.message.reply_markup, parse_mode="markdown")
+		bot.answer_callback_query(call.id, "Loading...")
+	if call.data == "More_Info":
+		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveTodaySortedByVendorAndCVSS(call.message.reply_to_message.text,"low"), reply_markup=call.message.reply_markup, parse_mode="markdown")
+		bot.answer_callback_query(call.id, "Loading...")
+	if call.data == "Products_Affected":
+		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=vulnerableProductsOrVendors(call.message.reply_to_message.text), reply_markup=call.message.reply_markup)
+		bot.answer_callback_query(call.id, "Loading...")
+	if call.data == "References":
+		bot.edit_message_text(message_id=call.message.id, chat_id=call.message.chat.id, text=cveReferences(call.message.reply_to_message.text), reply_markup=call.message.reply_markup)
 		bot.answer_callback_query(call.id, "Loading...")
 
 bot.infinity_polling() # Bot Exec
