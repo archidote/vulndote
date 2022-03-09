4115adcff3a2313cb5a393c8174dc61ca3197086
archidote <archidote.contact@gmail.com>
2022-03-09 14:46:01 +0100

    browse github.com, or sploitus.com - exploit browser - (exploitdb.com,github.com,0day.com  and other website which host exploit file. These features are not integrated to the bot at this moment

--- assets/controller.py
+++ assets/controller.py
@@ -4,10 +4,8 @@ import os
 oldURLApi = "https://cve.circl.lu/api/"
 vulndoteUser = os.environ.get('vulndoteUser')
 vulndotePassword = os.environ.get('vulndotePassword')
-telegramBotToken= os.environ.get('telegramBotToken')
-
-# print (vulndoteUser)
-# print (vulndotePassword)
+telegramBotToken = os.environ.get('telegramBotToken')
+GITHUB_ACCESS_TOKEN = os.environ.get('gihtubArchidoteTokenReadPublicRepos')
 
 session = requests.Session()
 session.auth = (vulndoteUser, vulndotePassword)
