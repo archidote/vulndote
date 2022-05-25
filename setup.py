f7d2d3f61a8ee94abc53544b39a3d4011990e331
archidote <archidote.contact@gmail.com>
2022-05-25 11:31:22 +0200

    python package pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vulndote",
+    version="0.0.1",
+    author="archidote",
+    author_email="archidote.contact@gmail.com",
+    url="https://github.com/archidote/vulndote",
+    description="Coming Soon",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["beautifulsoup4", "httpx", "PyGithub", "pyTelegramBotAPI", "requests", "schedule", "urllib3"],
+    entry_points={"console_scripts": ["timechecker = src.main:main"]},
+)
+
+setup()
